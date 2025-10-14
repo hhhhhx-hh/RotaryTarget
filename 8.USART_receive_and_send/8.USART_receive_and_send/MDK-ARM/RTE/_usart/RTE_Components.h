d1e2638b63b695c75dc0da11c12f6def92c7ac18
Chun-Ri-Chi <3341944823@qq.com>
2025-10-14 22:22:39 +0800

    添加了串口收发代码

---
+++ 8.USART_receive_and_send/8.USART_receive_and_send/MDK-ARM/RTE/_usart/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usart' 
+ * Target:  'usart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
